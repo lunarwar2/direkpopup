92bb9a71c9bb138837380e5c50940685bd66b86c
lunarwar2 <101807623+lunarwar2@users.noreply.github.com>
2022-12-16 21:07:12 +0700

    Create lq.js

---
+++ lq.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://hailofficemeasure.com/rupsfwt7q?key=2575bdae8f48b89e0a9447fe43c7e748', '_blank');
+	});
